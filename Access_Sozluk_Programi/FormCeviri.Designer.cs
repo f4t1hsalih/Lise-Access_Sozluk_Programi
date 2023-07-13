709c1353b1b9fb0cb113d1f168106eabd8871335
f4t1hsalih <f4t1hsalih@gmail.com>
2023-07-13 12:55:52 +0300

    Add project files.

---
+++ Access_Sozluk_Programi/FormCeviri.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Access_Sozluk_Programi
+{
+    partial class FormCeviri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCeviri));
+            this.rbtn1 = new System.Windows.Forms.RadioButton();
+            this.rbtn2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtarama = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rbtn1
+            // 
+            this.rbtn1.AutoSize = true;
+            this.rbtn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtn1.Location = new System.Drawing.Point(12, 12);
+            this.rbtn1.Name = "rbtn1";
+            this.rbtn1.Size = new System.Drawing.Size(71, 19);
+            this.rbtn1.TabIndex = 0;
+            this.rbtn1.TabStop = true;
+            this.rbtn1.Text = "TR-ENG";
+            this.rbtn1.UseVisualStyleBackColor = true;
+            // 
+            // rbtn2
+            // 
+            this.rbtn2.AutoSize = true;
+            this.rbtn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtn2.Location = new System.Drawing.Point(165, 12);
+            this.rbtn2.Name = "rbtn2";
+            this.rbtn2.Size = new System.Drawing.Size(71, 19);
+            this.rbtn2.TabIndex = 1;
+            this.rbtn2.TabStop = true;
+            this.rbtn2.Text = "ENG-TR";
+            this.rbtn2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(11, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Çeviriceğiniz kelimeyi giriniz:";
+            // 
+            // txtarama
+            // 
+            this.txtarama.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtarama.Location = new System.Drawing.Point(12, 63);
+            this.txtarama.Name = "txtarama";
+            this.txtarama.Size = new System.Drawing.Size(223, 21);
+            this.txtarama.TabIndex = 3;
+            this.txtarama.TextChanged += new System.EventHandler(this.txtarama_TextChanged);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID});
+            this.dgv.GridColor = System.Drawing.Color.DarkSeaGreen;
+            this.dgv.Location = new System.Drawing.Point(12, 89);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersWidth = 4;
+            this.dgv.Size = new System.Drawing.Size(224, 150);
+            this.dgv.TabIndex = 4;
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Visible = false;
+            // 
+            // Çeviri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(248, 251);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.txtarama);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rbtn2);
+            this.Controls.Add(this.rbtn1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Çeviri";
+            this.Text = "Çeviri";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbtn1;
+        private System.Windows.Forms.RadioButton rbtn2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtarama;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+    }
+}
+
---
+++ Access_Sozluk_Programi/FormCeviri.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace Access_Sozluk_Programi
+{
+    public partial class FormCeviri : Form
+    {
+        public FormCeviri()
+        {
+            InitializeComponent();
+        }
+
+        private void txtarama_TextChanged(object sender, EventArgs e)
+        {
+            if (rbtn1.Checked)
+            {
+                string yol = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source =" + Application.StartupPath + @"\\ceviri.accdb";
+                OleDbConnection cnn = new OleDbConnection();
+                OleDbCommand cmd = new OleDbCommand();
+                OleDbDataAdapter da = new OleDbDataAdapter();
+                DataTable dt = new DataTable();
+
+                cnn.ConnectionString = yol;
+                cmd.Connection = cnn;
+                cmd.CommandText = "SELECT * FROM ceviri2 WHERE Tur LIKE '" + txtarama.Text + "%'";
+
+                da.SelectCommand = cmd;
+                da.Fill(dt);
+                dgv.DataSource = dt;
+            }
+            else if (rbtn2.Checked)
+            {
+                string yol = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source =" + Application.StartupPath + @"\\ceviri.accdb";
+                OleDbConnection cnn = new OleDbConnection();
+                OleDbCommand cmd = new OleDbCommand();
+                OleDbDataAdapter da = new OleDbDataAdapter();
+                DataTable dt = new DataTable();
+
+                cnn.ConnectionString = yol;
+                cmd.Connection = cnn;
+                cmd.CommandText = "SELECT * FROM ceviri1 WHERE Ing LIKE '" + txtarama.Text + "%'";
+
+                da.SelectCommand = cmd;
+                da.Fill(dt);
+                dgv.DataSource = dt;
+            }
+        }
+    }
+}
